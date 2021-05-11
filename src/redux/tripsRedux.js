e7455e16723d599a4a78c16a664656f2ef3cedd2
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2021-05-11 19:25:10 +0200

    Edit tripRedux

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -13,7 +13,7 @@ export const getFilteredTrips = ({ trips, filters }) => {
 
   // TODO - filter by duration
   if (filters.duration) {
-    output = output.filter((trip) => trip.days >= filters.duration.to - filters.duration.from);
+    output = output.filter((trip) => trip.days >= filters.duration.from && trip.days <= filters.duration.to);
   }
 
   // TODO - filter by tags
