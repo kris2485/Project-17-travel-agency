7871518d733c8b850ceba90516a45ced15c243a5
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2021-05-15 18:17:38 +0200

    Add styles to OrderForm

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
-// import styles from './OrderForm.scss';
+import styles from './OrderForm.scss';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
 
 const OrderForm = ({ tripCost, options, setOrderOption }) => (
-  <Row>
+  <Row className={styles.box}>
     {pricing.map((option) => (
       <Col md={4} key={option.id}>
         <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption} />
