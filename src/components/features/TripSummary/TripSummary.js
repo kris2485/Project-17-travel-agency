d5e4645ee4ed9f9abf35d674e1b1b19f0fd856b8
Krzysztof Rajnert <krzysiekrajnert@gmail.com>
2021-05-22 19:16:58 +0200

    Add TripSummary.test

--- src/components/features/TripSummary/TripSummary.js
+++ src/components/features/TripSummary/TripSummary.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './TripSummary.scss';
-import {Col} from 'react-flexbox-grid';
+import { Col } from 'react-flexbox-grid';
 
-const TripSummary = ({id, image, name, cost, days, tags}) => (
+const TripSummary = ({ id, image, name, cost, days, tags }) => (
   <Col xs={12} sm={6} lg={4} className={styles.column}>
     <Link to={`/trip/${id}`} className={styles.link}>
       <article className={styles.component}>
         <img src={image} alt={name} />
         <h3 className={styles.title}>{name}</h3>
         <div className={styles.details}>
-          <span>{days} days</span>
-          <span>from {cost}</span>
+          <span className={styles.days}>{days} days</span>
+          <span className={styles.cost}>from {cost}</span>
         </div>
         <div className={styles.tags}>
-          {tags.map(tag => (
-            <span className={styles.tag} key={tag.toString()}>{tag}</span>
+          {tags.map((tag) => (
+            <span className={styles.tag} key={tag.toString()}>
+              {tag}
+            </span>
           ))}
         </div>
+        }
       </article>
     </Link>
   </Col>
@@ -31,7 +34,7 @@ TripSummary.propTypes = {
   intro: PropTypes.string,
   cost: PropTypes.string,
   days: PropTypes.number,
-  tags: PropTypes.array,
+  tags: PropTypes.array.isRequired,
 };
 
 export default TripSummary;
---
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import TripSummary from './TripSummary';
+
+describe('Component TripSummary', () => {
+  it('should render correct link address', () => {
+    const expectedLink = '/trip/abc';
+    const component = shallow(<TripSummary id='abc' />);
+
+    const renderedLink = component.find('.link').prop('to');
+    expect(renderedLink).toEqual(expectedLink);
+  });
+  it('should render correct src and alt', () => {
+    const expectedImage = 'image.jpg';
+    const expectedAlt = 'alt';
+    const component = shallow(<TripSummary image={expectedImage} name={expectedAlt} />);
+
+    const renderedImage = component.find('img').prop('src');
+    const renderedAlt = component.find('img').prop('alt');
+    expect(renderedImage).toEqual(expectedImage);
+    expect(renderedAlt).toEqual(expectedAlt);
+  });
+  it('should render correct name, cost and days', () => {
+    const expectedName = 'travel';
+    const expectedCost = '$500';
+    const expectedDays = 14;
+    const component = shallow(<TripSummary name={expectedName} cost={expectedCost} days={expectedDays} />);
+
+    const renderedName = component.find('.title').text();
+    const renderedCost = component.find('.cost').text();
+    const renderedDays = component.find('.days').text();
+    expect(renderedName).toEqual(expectedName);
+    expect(renderedCost).toEqual(renderedCost);
+    expect(renderedDays).toEqual(renderedDays);
+  });
+
+  it('should throw error without required props', () => {
+    expect(() => shallow(<TripSummary />)).toThrow();
+  });
+  it('should render tags', () => {
+    let expectedTags = ['tag-1', 'tag-2', 'tag-3'];
+    const component = shallow(<TripSummary tags={expectedTags} />);
+
+    const renderedFirstTag = component.find('.tag').at(0).text();
+    const renderedSecondTag = component.find('.tag').at(1).text();
+    const renderedThirdTag = component.find('.tag').at(2).text();
+
+    expect(renderedFirstTag).toEqual(expectedTags[0]);
+    expect(renderedSecondTag).toEqual(expectedTags[1]);
+    expect(renderedThirdTag).toEqual(expectedTags[2]);
+  });
+  it('should not render div with class tags if tags is undefined', () => {
+    const component = shallow(<TripSummary />);
+    expect(component.find('.tags')).toBe(true);
+  });
+});
